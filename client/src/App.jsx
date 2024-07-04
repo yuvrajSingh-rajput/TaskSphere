2b9fcb254b41b62771e078553ee1a8ab07a9bd3c
yuvraj_singh_rajput <161643356+yuvrajSingh-rajput@users.noreply.github.com>
2024-07-04 23:55:31 +0530

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Footer from "./components/Footer";
+import PageNotFound from "./components/PageNotFound";
+import Dashboard from "./components/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
+import axios from 'axios';
+import { AuthProvider } from "./contexts/Authcontext";
+import { Toaster } from 'react-hot-toast';
+
+axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.withCredentials = true;
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="main">
+          <Navbar />
+          <Toaster position="top-center" reverseOrder={true} toastOptions={{ duration: 2000 }} />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<PrivateRoute />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/profile" element={<Dashboard />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+              </Route>
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
